57be6e85dfac96a27d059ce78408003017b21d29
kotfind <kotfind@yandex.ru>
2024-11-20 21:52:19 +0300

    parse_atoms: parse value

--- db/tests/parse_atoms.cpp
+++ db/tests/parse_atoms.cpp
@@ -24,31 +24,3 @@ TEST(ws, parse_atoms)
         parse(ws1(), str);
     }
 END_TEST
-
-TEST(quoted_string, parse_atoms)
-    ASSERT_EQ(parse(string(), R"("abacaba")"), "abacaba");
-    ASSERT_EQ(parse(string(), R"("a\n\na\r\"c\taba")"), "a\n\na\r\"c\taba");
-END_TEST
-
-TEST(boolean, parse_atoms)
-    ASSERT_EQ(parse(boolean(), "TrUe"), true);
-    ASSERT_EQ(parse(boolean(), "FalSE"), false);
-END_TEST
-
-TEST(bytes, parse_atoms)
-    {
-        std::vector<std::byte> ans{std::byte{0x1a}, std::byte{0xbc}};
-        ASSERT_EQ(parse(bytes(), "0x1abc"), ans);
-    }
-
-    {
-        std::vector<std::byte> ans{std::byte{0x02}, std::byte{0x1a}, std::byte{0xbc}};
-        ASSERT_EQ(parse(bytes(), "0x21abc"), ans);
-    }
-END_TEST
-
-TEST(num, parse_atoms)
-    ASSERT_EQ(parse(num(), "42"), 42);
-    ASSERT_EQ(parse(num(), "-42"), -42);
-    ASSERT_EQ(parse(num(), "+42"), +42);
-END_TEST
