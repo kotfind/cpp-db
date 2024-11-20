17876f1299993f8e96fef4cc9c6b0c9b0fa884f0
kotfind <kotfind@yandex.ru>
2024-11-20 17:01:56 +0300

    ws parse atom

--- db/parse_atoms.hpp
+++ db/parse_atoms.hpp
@@ -4,27 +4,81 @@
 #include <optional>
 #include <system_error>
 
-PARSER(num_p)
-    try_cast(
-        view(seq(
-            opt(any(c('+'), c('-'))),
-            c('1', '9'),
-            rep(c('0', '9'))
-        )),
-        [](auto str) -> std::optional<int> {
-            int ans;
+namespace parse_atoms {
+    using namespace parser;
 
-            // from_chars refuses to parse leading +
-            if (str.starts_with('+')) {
-                str.remove_prefix(1);
-            }
+    inline auto num() {
+        return try_cast(
+            view(seq(
+                opt(any(c('+'), c('-'))),
+                c('1', '9'),
+                rep(c('0', '9'))
+            )),
+            [](auto str) -> std::optional<int> {
+                int ans;
 
-            auto res = std::from_chars(str.begin(), str.end(), ans);
-            if (res.ec == std::errc {}) {
-                return {ans};
-            } else {
-                return std::nullopt;
+                // from_chars refuses to parse leading +
+                if (str.starts_with('+')) {
+                    str.remove_prefix(1);
+                }
+
+                auto res = std::from_chars(str.begin(), str.end(), ans);
+                if (res.ec == std::errc {}) {
+                    return {ans};
+                } else {
+                    return std::nullopt;
+                }
             }
-        }
-    )
-END_PARSER
+        );
+    }
+
+    static auto ws_char() {
+        return any(
+            c(' '),
+            c('\n'),
+            c('\t'),
+            c('\r')
+        );
+    }
+
+    static auto comment() {
+        return any(
+            // /* comment */
+            ignore(seq(
+                s("/*"),
+                rep(seq(
+                    neg(s("*/")),
+                    c()
+                )),
+                s("*/")
+            )),
+            // // comment
+            ignore(seq(
+                s("//"),
+                rep(seq(
+                    neg(s("\n")),
+                    c()
+                )),
+                s("\n")
+            ))
+        );
+    }
+
+    inline auto ws() {
+        return ignore(
+            rep(any(
+                ignore(ws_char()),
+                ignore(comment())
+            ))
+        );
+    }
+
+    inline auto ws1() {
+        return ignore(
+            rep1(any(
+                ignore(ws_char()),
+                ignore(comment())
+            ))
+        );
+    }
+}
--- db/tests/parse_atoms.cpp
+++ db/tests/parse_atoms.cpp
@@ -2,8 +2,29 @@
 
 #include "parse_atoms.hpp"
 
-TEST(num_p_)
-    ASSERT_EQ(parse(num_p(), "42"), 42);
-    ASSERT_EQ(parse(num_p(), "-42"), -42);
-    ASSERT_EQ(parse(num_p(), "+42"), +42);
+using namespace parse_atoms;
+
+TEST_GROUP(parse_atoms_)
+
+TEST(num_, parse_atoms_)
+    ASSERT_EQ(parse(num(), "42"), 42);
+    ASSERT_EQ(parse(num(), "-42"), -42);
+    ASSERT_EQ(parse(num(), "+42"), +42);
+END_TEST
+
+TEST(ws_, parse_atoms_)
+    std::vector<std::string> strs = {
+        "   ",
+        " \n \t ",
+        "/* hello */",
+        "  \n/* he\nllo */ ",
+        "  \n// hello\n /* world */",
+    };
+
+    parse(ws(), "");
+
+    for (const auto& str : strs) {
+        parse(ws(), str);
+        parse(ws1(), str);
+    }
 END_TEST
