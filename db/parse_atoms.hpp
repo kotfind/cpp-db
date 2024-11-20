90d0a85b52f57adc30c3022f5f6b9232344d6864
kotfind <kotfind@yandex.ru>
2024-11-20 21:37:01 +0300

    parse_atoms: bool, string, bytes

--- db/parse_atoms.hpp
+++ db/parse_atoms.hpp
@@ -1,38 +1,14 @@
 #include "parser.hpp"
 
 #include <charconv>
+#include <cstddef>
 #include <optional>
 #include <system_error>
 
 namespace parse_atoms {
     using namespace parser;
 
-    inline auto num() {
-        return try_cast(
-            view(seq(
-                opt(any(c('+'), c('-'))),
-                c('1', '9'),
-                rep(c('0', '9'))
-            )),
-            [](auto str) -> std::optional<int> {
-                int ans;
-
-                // from_chars refuses to parse leading +
-                if (str.starts_with('+')) {
-                    str.remove_prefix(1);
-                }
-
-                auto res = std::from_chars(str.begin(), str.end(), ans);
-                if (res.ec == std::errc {}) {
-                    return {ans};
-                } else {
-                    return std::nullopt;
-                }
-            }
-        );
-    }
-
-    static auto ws_char() {
+    inline auto ws_char() {
         return any(
             c(' '),
             c('\n'),
@@ -41,7 +17,7 @@ namespace parse_atoms {
         );
     }
 
-    static auto comment() {
+    inline auto comment() {
         return any(
             // /* comment */
             ignore(seq(
@@ -65,20 +41,102 @@ namespace parse_atoms {
     }
 
     inline auto ws() {
-        return ignore(
-            rep(any(
-                ignore(ws_char()),
-                ignore(comment())
-            ))
-        );
+        return ignore(rep(any(
+            ignore(ws_char()),
+            ignore(comment())
+        )));
     }
 
     inline auto ws1() {
-        return ignore(
-            rep1(any(
-                ignore(ws_char()),
-                ignore(comment())
-            ))
+        return ignore(rep1(any(
+            ignore(ws_char()),
+            ignore(comment())
+        )));
+    }
+
+    inline auto string_char() {
+        return any(
+            cast(s("\\n"), [](auto) { return '\n'; }),
+            cast(s("\\r"), [](auto) { return '\r'; }),
+            cast(s("\\t"), [](auto) { return '\t'; }),
+            cast(s("\\\\"), [](auto) { return '\\'; }),
+            cast(s("\\\""), [](auto) { return '\"'; }),
+            seq(neg(c('"')), c())
+        );
+    }
+
+    inline auto string() {
+        return cast(
+            seq(
+                ignore(c('"')),
+                rep(string_char()), // str
+                ignore(c('"'))
+            ),
+            [](auto str) {
+                return std::string(str.begin(), str.end());
+            }
+        );
+    }
+
+    inline auto boolean() {
+        return any(
+            cast(S("true"), [](auto) { return true; }),
+            cast(S("false"), [](auto) { return false; })
+        );
+    }
+
+    inline auto bytes() {
+        return cast(
+            seq(
+                ignore(s("0x")),
+                rep1(any(
+                    cast(c('0', '9'), [](char ch) { return (int)(ch - '0'); }),
+                    cast(c('a', 'z'), [](char ch) { return (int)(ch - 'a' + 10); })
+                ))
+            ),
+            [](auto digits16) {
+                std::vector<std::byte> ans;
+                ans.reserve((digits16.size() + 1) / 2);
+                auto it = digits16.begin();
+                if (digits16.size() % 2 == 1) {
+                    ans.push_back(std::byte(*it));
+                    ++it;
+                }
+                while (it != digits16.end()) {
+                    auto h = *it;
+                    ++it;
+                    auto l = *it;
+                    ++it;
+                    ans.push_back(std::byte(h * 16 + l));
+                }
+                return ans;
+            }
+        );
+    }
+
+    inline auto num() {
+        return try_cast(
+            view(seq(
+                neg(s("0x")),
+                opt(any(c('+'), c('-'))),
+                c('1', '9'),
+                rep(c('0', '9'))
+            )),
+            [](auto str) -> std::optional<int> {
+                int ans;
+
+                // from_chars refuses to parse leading +
+                if (str.starts_with('+')) {
+                    str.remove_prefix(1);
+                }
+
+                auto res = std::from_chars(str.begin(), str.end(), ans);
+                if (res.ec == std::errc {}) {
+                    return {ans};
+                } else {
+                    return std::nullopt;
+                }
+            }
         );
     }
 }
--- db/tests/parse_atoms.cpp
+++ db/tests/parse_atoms.cpp
@@ -2,15 +2,11 @@
 
 #include "parse_atoms.hpp"
 
+#include <cstddef>
+
 using namespace parse_atoms;
 
 TEST_GROUP(parse_atoms)
-
-TEST(num, parse_atoms)
-    ASSERT_EQ(parse(num(), "42"), 42);
-    ASSERT_EQ(parse(num(), "-42"), -42);
-    ASSERT_EQ(parse(num(), "+42"), +42);
-END_TEST
 
 TEST(ws, parse_atoms)
     std::vector<std::string> strs = {
@@ -27,4 +23,32 @@ TEST(ws, parse_atoms)
         parse(ws(), str);
         parse(ws1(), str);
     }
+END_TEST
+
+TEST(quoted_string, parse_atoms)
+    ASSERT_EQ(parse(string(), R"("abacaba")"), "abacaba");
+    ASSERT_EQ(parse(string(), R"("a\n\na\r\"c\taba")"), "a\n\na\r\"c\taba");
+END_TEST
+
+TEST(boolean, parse_atoms)
+    ASSERT_EQ(parse(boolean(), "TrUe"), true);
+    ASSERT_EQ(parse(boolean(), "FalSE"), false);
+END_TEST
+
+TEST(bytes, parse_atoms)
+    {
+        std::vector<std::byte> ans{std::byte{0x1a}, std::byte{0xbc}};
+        ASSERT_EQ(parse(bytes(), "0x1abc"), ans);
+    }
+
+    {
+        std::vector<std::byte> ans{std::byte{0x02}, std::byte{0x1a}, std::byte{0xbc}};
+        ASSERT_EQ(parse(bytes(), "0x21abc"), ans);
+    }
+END_TEST
+
+TEST(num, parse_atoms)
+    ASSERT_EQ(parse(num(), "42"), 42);
+    ASSERT_EQ(parse(num(), "-42"), -42);
+    ASSERT_EQ(parse(num(), "+42"), +42);
 END_TEST
