394c1e0f60ffbebaf23c221764922d854b87d3ab
kotfind <kotfind@yandex.ru>
2024-11-22 03:19:57 +0300

    parser: double indirection parser

--- parser/parser.hpp
+++ parser/parser.hpp
@@ -5,6 +5,7 @@
 #include "concepts.hpp"
  
 #include <memory>
+#include <stdexcept>
 #include <string_view>
 #include <type_traits>
 
@@ -26,7 +27,6 @@ namespace parser {
             template<is_parser_for<T> P>
             requires std::is_same<typename P::type, T>::value
             class Inner : public AbstractParser {
-
                 public:
                     Inner(P parser_)
                       : parser(std::move(parser_))
@@ -43,13 +43,47 @@ namespace parser {
         public:
             template<is_parser_for<T> P>
             Parser(P parser_)
-              : parser(std::make_shared<Inner<P>>(std::move(parser_)))
+              : parser(
+                    std::make_shared<std::unique_ptr<AbstractParser>>(
+                        std::unique_ptr<AbstractParser>(
+                            std::make_unique<Inner<P>>(std::move(parser_))
+                        )
+                    )
+                )
+            {}
+
+            /// Creates "dummy" parser. Useful for creating recursive parsers
+            Parser()
+              : parser(
+                    std::make_shared<std::unique_ptr<AbstractParser>>(
+                        std::unique_ptr<AbstractParser>(nullptr)
+                    )
+                )
             {}
 
             result parse(std::string_view s) const {
-                return parser->parse(s);
+                if (is_initialized()) {
+                    return (*parser)->parse(s);
+                } else {
+                    throw std::runtime_error("parser was not initialized");
+                }
             }
 
-            std::shared_ptr<AbstractParser> parser;
+            template<is_parser_for<T> P>
+            void initialize(P parser_) {
+                if (is_initialized()) {
+                    throw std::runtime_error("parser was already initialized");
+                }
+                parser->reset(new Inner<P>(std::move(parser_)));
+            }
+
+        private:
+
+            bool is_initialized() const {
+                return parser->get() != nullptr;
+            }
+
+            // Double indirection allows creating recursive parsers
+            std::shared_ptr<std::unique_ptr<AbstractParser>> parser;
     };
 }
--- parser/tests/parser.cpp
+++ parser/tests/parser.cpp
@@ -1,13 +1,44 @@
 #include "test_utils.hpp"
 
+#include "any.hpp"
+#include "cast.hpp"
+#include "ignore.hpp"
+#include "sequence.hpp"
+#include "string.hpp"
+
 #include "parser.hpp"
 #include "parse.hpp"
 #include "char.hpp"
 
 using namespace parser;
 
-TEST(parser)
-    Parser<char> p = c('a', 'z');
+TEST_GROUP(parser)
+    TEST(simple, parser)
+        Parser<char> p = c('a', 'z');
 
-    ASSERT_EQ(parse(p, "a"), 'a');
-END_TEST
+        ASSERT_EQ(parse(p, "a"), 'a');
+    END_TEST
+
+    TEST(recursive, parser)
+        Parser<size_t> braces;
+        braces.initialize(any(
+            cast(
+                seq(
+                    ignore(c('(')),
+                    braces,
+                    ignore(c(')'))
+                ),
+                [](auto num) -> size_t {
+                    return num + 1;
+                }
+            ),
+            cast(
+                s(""),
+                [](auto) -> size_t { return 0; }
+            )
+        ));
+
+        ASSERT_EQ(parse(braces, ""), 0);
+        ASSERT_EQ(parse(braces, "()"), 1);
+        ASSERT_EQ(parse(braces, "((()))"), 3);
+    END_TEST
